90f6891ab6f2e37ee1367cc3539a25f9ef0db221
mcproger <artem7lomonosov@gmail.com>
2017-06-30 14:46:43 +0300

    Add heroku settings

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from argparse import ArgumentParser
+import os
 
 app = Flask(__name__)
+
+def parse_argv():
+    parser = ArgumentParser(description="Starts flask server.")
+    parser.add_argument('--host', '-H', type=str, default="0.0.0.0",
+                        help="Specify host id.")
+    parser.add_argument('--port', '-p', type=int, default=int(os.environ.get('PORT', 5000)),
+                        help="Specify local port, where to start server.")
+    parser.add_argument('--debug', '-d', type=bool, default=True,
+                        help="Start server in debug mode.")
+    return parser.parse_args()
 
 @app.route('/')
 def webhook():
@@ -12,4 +24,5 @@ def vk_confirmation():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    args = parse_argv()
+    app.run(host=args.host, port=args.port, debug=args.debug)
