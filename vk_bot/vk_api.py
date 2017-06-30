b54f2ac63566cbfed659540e0bd1b219b00f5697
Ilya Osipov <070809010@mail.ru>
2017-07-01 00:52:16 +0300

    Implement get_user_info and send_message methods

--- vk_bot/vk_api.py
+++ vk_bot/vk_api.py
@@ -1,23 +1,45 @@
+import vk
+
+session = vk.Session()
+api = vk.API(session, v=5.0)
+
+
+def send_message(user_id, token, message, attachment=""):
+    api.messages.send(access_token=token, user_id=str(user_id), message=message)
+
+
+def get_user_info(letter_id):
+    user_info = api.users.get(user_ids=letter_id)
+    return user_info[0]
+
+
 def get_user_id():
     pass
+
 
 def get_group_id():
     pass
 
+
 def parse_fresh_news():
     pass
+
 
 def parse_fresh_memes():
     pass
 
+
 def parse_possible_photos():
     pass
+
 
 def parse_hidden_info():
     pass
 
+
 def fetch_friends_list():
     pass
+
 
 def fetch_groups_list():
     pass
