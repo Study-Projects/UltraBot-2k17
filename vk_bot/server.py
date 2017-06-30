8a3ebdc2b0a1463fc8fdf2739c8c0867abdf142c
mcproger <artem7lomonosov@gmail.com>
2017-06-30 14:33:14 +0300

    Add Flask start settings

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -1,7 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 @app.route('/')
 def webhook():
-    pass
+    return "Hello world"
 
 @app.route('/', methods=['POST'])
 def vk_confirmation():
     pass
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
