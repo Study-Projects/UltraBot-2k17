63d26b247d33e15f648a7938f5b1958ae50d17f7
Ilya Osipov <070809010@mail.ru>
2017-06-30 23:12:41 +0300

    Delete useless arguments

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -7,7 +7,7 @@ def delete_mem_group_handler():
 def post_memes_handler():
     pass
 
-def post_memes_from_handler(group):
+def post_memes_from_handler():
     pass
 
 def add_news_group_handler():
@@ -19,20 +19,14 @@ def delete_news_group_handler():
 def post_news_handler():
     pass
 
-def post_news_from_handler(group):
+def post_news_from_handler():
     pass
 
-def add_friend_to_stalker_handler(friend):
+def parse_possible_photos_handler():
     pass
 
-def delete_friend_from_stalker_handler(friend):
+def imitate_newsfeed_handler():
     pass
 
-def parse_possible_photos_handler(friend):
-    pass
-
-def imitate_newsfeed_handler(friend):
-    pass
-
-def parse_hidden_info_handler(friend):
+def parse_hidden_info_handler():
     pass
