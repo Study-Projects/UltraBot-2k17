44b3eee4578220b66d7406d2db11cdc374d83cdb
Ilya Osipov <070809010@mail.ru>
2017-07-01 01:02:13 +0300

    Add datatype check into webhook

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -1,64 +1,72 @@
-from flask import Flask
+from flask import Flask,request,json
 import os
-from config import CONFIRMATION_TOKEN
+from config import CONFIRMATION_TOKEN, TOKEN
 from vk_bot.messenger import message_validators,message_handlers
+from vk_bot import vk_api
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-    messaging_event = ''
+    data = json.loads(request.data)
+    if data['type'] == 'confirmation':
+        return CONFIRMATION_TOKEN
+    elif data['type'] != 'message_new':
+        return 
+    letter_id = data['object']['user_id']
+    user_info = vk_api.get_user_info(letter_id)
+    vk_response = data['object']['body'].lower()
     message_processors = [
         (
-            message_validators.is_add_mem_group_command(messaging_event),
+            message_validators.is_add_mem_group_command(vk_response),
             message_handlers.add_mem_group_handler()
         ),
         (
-            message_validators.is_delete_mem_group_command(messaging_event),
+            message_validators.is_delete_mem_group_command(vk_response),
             message_handlers.delete_mem_group_handler()
         ),
         (
-            message_validators.is_post_memes_command(messaging_event),
+            message_validators.is_post_memes_command(vk_response),
             message_handlers.post_memes_handler()
         ),
         (
-            message_validators.is_post_memes_from_command(messaging_event),
+            message_validators.is_post_memes_from_command(vk_response),
             message_handlers.post_memes_from_handler()
         ),
         (
-            message_validators.is_add_news_group_command(messaging_event),
+            message_validators.is_add_news_group_command(vk_response),
             message_handlers.add_mem_group_handler()
         ),
         (
-            message_validators.is_delete_news_group_command(messaging_event),
+            message_validators.is_delete_news_group_command(vk_response),
             message_handlers.delete_news_group_handler()
         ),
         (
-            message_validators.is_post_news_command(messaging_event),
+            message_validators.is_post_news_command(vk_response),
             message_handlers.post_news_handler()
         ),
         (
-            message_validators.is_post_news_from_command(messaging_event),
+            message_validators.is_post_news_from_command(vk_response),
             message_handlers.post_news_from_handler()
         ),
         (
-            message_validators.is_parse_possible_photos_command(messaging_event),
+            message_validators.is_parse_possible_photos_command(vk_response),
             message_handlers.parse_possible_photos_handler()
         ),
         (
-            message_validators.is_imitate_newsfeed_command(messaging_event),
+            message_validators.is_imitate_newsfeed_command(vk_response),
             message_handlers.imitate_newsfeed_handler()
         ),
         (
-            message_validators.is_parse_hidden_info_command(messaging_event),
+            message_validators.is_parse_hidden_info_command(vk_response),
             message_handlers.parse_hidden_info_handler()
         )
     ]
     for message_validator, message_handler in message_processors:
-        if message_validator(messaging_event):
-            message_handler()
-    return CONFIRMATION_TOKEN
+        if message_validator(vk_response):
+            message_handler(user_info, TOKEN)
+    return 'ok'
 
 
 if __name__ == "__main__":
