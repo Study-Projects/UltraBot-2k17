bf4ee4e1f5c7c08c445f8063aa112cb9a51eb014
mcproger <artem7lomonosov@gmail.com>
2017-06-30 15:24:42 +0300

    Fix server.py

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -1,18 +1,9 @@
 from flask import Flask
-from argparse import ArgumentParser
 import os
 
 app = Flask(__name__)
 
-def parse_argv():
-    parser = ArgumentParser(description="Starts flask server.")
-    parser.add_argument('--host', '-H', type=str, default="0.0.0.0",
-                        help="Specify host id.")
-    parser.add_argument('--port', '-p', type=int, default=int(os.environ.get('PORT', 5000)),
-                        help="Specify local port, where to start server.")
-    parser.add_argument('--debug', '-d', type=bool, default=True,
-                        help="Start server in debug mode.")
-    return parser.parse_args()
+
 
 @app.route('/')
 def webhook():
@@ -24,5 +15,5 @@ def vk_confirmation():
 
 
 if __name__ == "__main__":
-    args = parse_argv()
-    app.run(host=args.host, port=args.port, debug=args.debug)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
