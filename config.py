5869faa3e272bfea30916f0eca63093e4de6643c
mcproger <artem7lomonosov@gmail.com>
2017-06-30 18:53:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DATABASE_PATH = ''
-BOT_GROUP_ID = ''
-TOKEN = ''
-CONFIRMATION_TOKEN = ''
+BOT_GROUP_ID = '149540554' #replace with os module
+TOKEN = '1e73594c0338ba47420060743b19864f16f3f8afe74da187facfcd8e5ddd7456f166efea6ed6d3a28c555' #replace with os module
+CONFIRMATION_TOKEN = 'd30692a9' #replace with os module
