d880388119bb7dccf2cd6053c539bdf2d2a9e725
Ilya Osipov <070809010@mail.ru>
2017-07-01 03:07:24 +0300

    Add all needed vk_api methods

--- vk_bot/vk_api.py
+++ vk_bot/vk_api.py
@@ -4,42 +4,42 @@ session = vk.Session()
 api = vk.API(session, v=5.0)
 
 
-def send_message(user_id, token, message, attachment=""):
+def send_message(user_id, token, message):
     api.messages.send(access_token=token, user_id=str(user_id), message=message)
 
 
-def get_user_info(letter_id):
-    user_info = api.users.get(user_ids=letter_id)
+def get_user_info(user_letter_id):
+    user_info = api.users.get(user_ids=user_letter_id)
+    if 'error' in user_info:
+        return None
     return user_info[0]
 
 
-def get_user_id():
-    pass
+def get_group_info(group_letter_id):
+    group_info = api.groups.getById(group_ids=group_letter_id)
+    if 'error' in group_info:
+        return None
+    return group_info[0]
 
 
-def get_group_id():
-    pass
+def fetch_friends_list(user_id):
+    friends_list = api.friends.get(user_id=user_id)
+    if 'error' in friends_list:
+        return None
+    return friends_list['response']
 
 
-def parse_fresh_news():
-    pass
+def fetch_groups_list(user_id):
+    groups_list = api.groups.get(user_id=user_id)
+    if 'error' in groups_list:
+        return None
+    return groups_list['response']
 
 
-def parse_fresh_memes():
-    pass
-
-
-def parse_possible_photos():
-    pass
-
-
-def parse_hidden_info():
-    pass
-
-
-def fetch_friends_list():
-    pass
-
-
-def fetch_groups_list():
-    pass
+def parse_posts(is_user_posts, id):
+    if not is_user_posts:
+        id = '-' + str(id)
+    posts = api.wall.get(owner_id=id)
+    if 'error' in posts:
+        return None
+    return posts['response']
