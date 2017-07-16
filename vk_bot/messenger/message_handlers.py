a0c569f22d8dbe7449d4da1eafc63273f50827e4
mcproger <artem7lomonosov@gmail.com>
2017-07-16 22:57:34 +0300

    Add db add mem group handler

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -3,13 +3,15 @@ from vk_bot.server import db
 from vk_bot.models.groups import Mems
 
 def add_mem_group_handler(user_info, TOKEN, vk_response):
-    db.session.add(Mems(vk_response.split()[2], vk_response.split()[3]))
+    group_name = vk_response.split()[2]
+    group_id = vk_response.split()[3]
+    db.session.add(Mems(group_name, group_id))
     db.session.commit()
     message = "Группа добавлена" 
     return vk_api.send_message(user_info, TOKEN, message) 
 
 
-def delete_mem_group_handler(user_info, token):
+def delete_mem_group_handler(user_info, token, vk_response):
     pass
 
 
