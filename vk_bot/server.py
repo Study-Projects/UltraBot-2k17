c7c43ebd07c6ce1392dbb7d724f6ff8bcd81a121
mcproger <artem7lomonosov@gmail.com>
2017-07-16 19:10:10 +0300

    Some fixes

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -11,7 +11,67 @@ from vk_bot.messenger import message_validators,message_handlers
 @app.route('/', methods=['GET', 'POST'])
 def webhook():
     data = json.loads(request.data)
-    return data
+    if data['type'] == 'confirmation':
+        return CONFIRMATION_TOKEN
+    elif data['type'] != 'message_new':
+        return
+    letter_id = data['object']['user_id']
+    user_info = vk_api.get_user_info(letter_id)
+    vk_response = data['object']['body'].lower()
+    message_processors = [
+        (
+            message_validators.is_add_mem_group_command,
+            message_handlers.add_mem_group_handler
+        ),
+        (
+            message_validators.is_delete_mem_group_command,
+            message_handlers.delete_mem_group_handler
+        ),
+        (
+            message_validators.is_post_memes_command,
+            message_handlers.post_memes_handler
+        ),
+        (
+            message_validators.is_post_memes_from_command,
+            message_handlers.post_memes_from_handler
+        ),
+        (
+            message_validators.is_add_news_group_command,
+            message_handlers.add_mem_group_handler
+        ),
+        (
+            message_validators.is_delete_news_group_command,
+            message_handlers.delete_news_group_handler
+        ),
+        (
+            message_validators.is_post_news_command,
+            message_handlers.post_news_handler
+        ),
+        (
+            message_validators.is_post_news_from_command,
+            message_handlers.post_news_from_handler
+        ),
+        (
+            message_validators.is_parse_possible_photos_command,
+            message_handlers.parse_possible_photos_handler
+        ),
+        (
+            message_validators.is_imitate_newsfeed_command,
+            message_handlers.imitate_newsfeed_handler
+        ),
+        (
+            message_validators.is_parse_hidden_info_command,
+            message_handlers.parse_hidden_info_handler
+        ),
+        (
+            message_validators.is_post_weater_command,
+            message_handlers.post_weater
+        )
+    ]
+    for message_validator, message_handler in message_processors:
+        if message_validator(vk_response):
+            message_handler(user_info, TOKEN, vk_response)
+    return 'ok'
 
 
 if __name__ == "__main__":
