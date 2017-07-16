1e3a0b1bb62de849e01a1c96102924a4a8237fd9
mcproger <artem7lomonosov@gmail.com>
2017-07-16 21:52:13 +0300

    Some fixes

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -5,6 +5,9 @@ from config import CONFIRMATION_TOKEN, TOKEN
 from vk_bot import vk_api
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
 from vk_bot.messenger import message_validators,message_handlers
 
