3703e5667ea96190ccd2eb443345eeca71d264c1
mcproger <artem7lomonosov@gmail.com>
2017-07-16 20:18:28 +0300

    Fix db import

--- vk_bot/models/groups.py
+++ vk_bot/models/groups.py
@@ -1,9 +1,9 @@
-from vk_bot.models import base
+from vk_bot.models.base import db
 
-class News(base.db.Model):
-    id = base.db.Column(db.Integer, primary_key=True, autoincrement=True)
-    group_name = base.db.Column(db.String(128), unique=True, nullable=False)
-    group_id = base.db.Column(db.String(128), unique=True, nullable=False)
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    group_name = db.Column(db.String(128), unique=True, nullable=False)
+    group_id = db.Column(db.String(128), unique=True, nullable=False)
 
     def __init__(self, group_name, group_id):
         self.group_name = group_name
@@ -13,10 +13,10 @@ class News(base.db.Model):
         return '<News %r, %r>' % (self.group_name, self.group_id)
 
 
-class Mems(base.db.Model):
-    id = base.db.Column(db.Integer, primary_key=True, autoincrement=True)
-    group_name = base.db.Column(db.String(128), unique=True, nullable=False)
-    group_id = base.db.Column(db.String(128), unique=True, nullable=False)
+class Mems(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    group_name = db.Column(db.String(128), unique=True, nullable=False)
+    group_id = db.Column(db.String(128), unique=True, nullable=False)
 
     def __init__(self, group_name, group_id):
         self.group_name = group_name
