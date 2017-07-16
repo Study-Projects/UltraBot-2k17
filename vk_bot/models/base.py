3a9c4ebcae6e82b51153a7a6697afeade0b50949
mcproger <artem7lomonosov@gmail.com>
2017-07-16 19:33:21 +0300

    Some fixes

--- vk_bot/models/base.py
+++ vk_bot/models/base.py
@@ -1,8 +1,8 @@
-from vk_bot import server 
+from vk_bot.server import app 
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-server.app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-server.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(server.app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
