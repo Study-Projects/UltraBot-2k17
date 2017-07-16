55fbf1a5c2d013743c015e2645acfc703fd3e6af
mcproger <artem7lomonosov@gmail.com>
2017-07-16 22:32:59 +0300

    Fix db mem group handler

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -3,9 +3,7 @@ from vk_bot.server import db
 from vk_bot.models.groups import Mems
 
 def add_mem_group_handler(user_info, TOKEN, vk_response):
-    group_name = vk_response.split()[2]
-    group_id = vk_response.split()[3]
-    db.session.add(Mems(group_name, group_id))
+    db.session.add(Mems(vk_response.split()[2], vk_response.split()[3]))
     db.session.commit()
     message = "Группа добавлена" 
     return vk_api.send_message(user_info, TOKEN, message) 
