250b68c1fe1530b7f381b78ec2b565ff9564f7b2
mcproger <artem7lomonosov@gmail.com>
2017-07-16 19:38:05 +0300

    Some fixes

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -5,7 +5,7 @@ from vk_bot.models import base
 def add_mem_group_handler(user_info, TOKEN, vk_response):
     group_name = vk_response.split()[2]
     group_id = vk_response.split()[3]
-    base.db.session.add(groups.Mems(group_name, group_id))
+    base.db.session.add(Mems(group_name, group_id))
     base.db.session.commit()
     return vk_api.send_message(user_info, TOKEN, "Group added") 
 
