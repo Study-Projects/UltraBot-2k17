0eeac3355b5aa94d263fb5616aada6b8e685bed7
Ilya Osipov <070809010@mail.ru>
2017-07-18 03:22:39 +0500

    Fix ref

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -3,6 +3,6 @@ import json
 
 
 def fetch_weather(weather_key, city):
-    url = 'https: // api.apixu.com / v1 / current.json?key = %s & q = %s', (weather_key, city)
+    url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s', (weather_key, city)
     response = urllib.request.urlopen(url).read().decode('utf-8')
     return json.loads(response)
