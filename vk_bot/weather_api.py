670e6380d75b5efcef915429730eb37e48178892
Ilya Osipov <070809010@mail.ru>
2017-07-18 15:50:15 +0500

    Fix syntax error

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -3,6 +3,6 @@ import urllib.request
 
 
 def fetch_weather(weather_key, city):
-    url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s', (weather_key, city)
+    url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s' % (weather_key, city)
     site = urllib.request.urlopen(url)
     return site.read()
