e1dd2dd190b536471e100570fbbf7de957719722
mcproger <artem7lomonosov@gmail.com>
2017-07-18 15:31:20 +0300

    Some fixes

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -23,9 +23,10 @@ def delete_mem_group_handler(user_info, TOKEN, vk_response):
 
     
 def post_memes_handler(user_info, TOKEN, vk_response):
-    groups_id = Mems.query.order_by(Mems.group_id)
-    for group_id in groups_id:
-        vk_api.send_message(user_info, TOKEN, str(group_id)[16:-1])
+    groups_letter_id = Mems.query.order_by(Mems.group_id)
+    for group_letter_id in groups_letter_id:
+        group_number_id = vk_api.get_group_info(str(group_letter_id)[16:-1])
+        vk_api.send_message(user_info, TOKEN, group_number_id) 
 
 
 def post_memes_from_handler(user_info, token):
