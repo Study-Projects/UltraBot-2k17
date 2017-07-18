15189a6bac01a3b4475339ff7d0d94028c37bb8d
mcproger <artem7lomonosov@gmail.com>
2017-07-18 20:51:43 +0300

    Add vk environment variables

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
+APP_ID = os.environ['APP_ID']
+LOGIN = os.environ['LOGIN']
+PASSWORD = os.environ['PASSWORD']
 BOT_GROUP_ID = os.environ['BOT_GROUP_ID']
 TOKEN = os.environ['TOKEN']
 CONFIRMATION_TOKEN = os.environ['CONFIRMATION_TOKEN']
