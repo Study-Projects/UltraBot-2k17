2ee7737aba71012ee11c6661a8888b78117660b8
Ilya Osipov <070809010@mail.ru>
2017-07-18 14:17:02 +0500

    Refctor fetch_weather()

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -4,5 +4,7 @@ import json
 
 def fetch_weather(weather_key, city):
     url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s', (weather_key, city)
-    response = urllib.request.urlopen(url).read().decode('utf-8')
-    return json.loads(response)
+    with urllib.request.urlopen(url) as opened_url:
+        data = json.loads(opened_url.read().decode())
+        return data
+    return
