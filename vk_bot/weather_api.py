9968df96bbe820120642d3fd100203a0810c0c63
mcproger <artem7lomonosov@gmail.com>
2017-07-19 00:26:00 +0300

    Fix syntax mistake

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -3,6 +3,6 @@ import json
 
 
 def fetch_weather(weather_key, city):
-    url = 'https: // api.apixu.com / v1 / current.json?key = %s & q = %s', (weather_key, city)
-    response = urllib.request.urlopen(url).read().decode('utf-8')
-    return json.loads(response)
+    url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s' % (weather_key, city)
+    site = urllib.request.urlopen(url)
+    return site.read()
