10da30faf96120ac1f1733c936773fb61b1efcbc
Ilya Osipov <070809010@mail.ru>
2017-07-18 14:40:09 +0500

    Fix response reading

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -1,10 +1,8 @@
 import urllib.request
-import json
 
 
 def fetch_weather(weather_key, city):
     url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s', (weather_key, city)
-    with urllib.request.urlopen(url) as opened_url:
-        data = json.loads(opened_url.read().decode())
+    with urllib.request.urlopen(url) as site:
+        data = site.read()
         return data
-    return
