45426486b33af2e5d84473aa6d4df15dbd518bd8
voron434 <070809010@mail.ru>
2017-07-18 13:59:46 +0500

    Revert stupid commit

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -51,7 +51,6 @@ def parse_hidden_info_handler(user_info, token):
     pass
 
 
-def post_weater(user_info, TOKEN, vk_response):
-    city = vk_response.split()[-1]
-    weather_info = weather_api.fetch_weather(WEATHER_KEY, city)
-    return vk_api.send_message(user_info, weather_info)
+def post_weater(user_info, token):
+    pass
+
