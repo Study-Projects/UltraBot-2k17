4b8d6dfa8724557dd5d04a2cc7418eaef014f16b
Ilya Osipov <070809010@mail.ru>
2017-07-18 15:03:28 +0500

    Try another way to get info from website

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -1,8 +1,8 @@
 import urllib.request
 
 
+
 def fetch_weather(weather_key, city):
     url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s', (weather_key, city)
-    with urllib.request.urlopen(url) as site:
-        data = site.read()
-        return data
+    site = urllib.request.urlopen(url)
+    return site.read()
