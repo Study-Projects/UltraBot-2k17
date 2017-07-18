7074a1f5726f16b02f4666a7d65c8e7750a2fb4b
mcproger <artem7lomonosov@gmail.com>
2017-07-18 19:00:40 +0300

    Add vk_user_api and vk_group_api

--- vk_bot/messenger/message_handlers.py
+++ vk_bot/messenger/message_handlers.py
@@ -1,4 +1,5 @@
-from vk_bot import vk_api
+from vk_bot import vk_group_api
+from vk_bot import vk_user_api
 from vk_bot.server import db
 from vk_bot.models.groups import Mems
 
@@ -8,27 +9,27 @@ def add_mem_group_handler(user_info, TOKEN, vk_response):
     db.session.add(Mems(group_name, group_id))
     db.session.commit()
     message = "Группа добавлена" 
-    return vk_api.send_message(user_info, TOKEN, message) 
+    return vk_group_api.send_message(user_info, TOKEN, message) 
 
 
 def delete_mem_group_handler(user_info, TOKEN, vk_response):
     group = Mems.query.filter_by(group_name=vk_response.split()[2]).first()
     if group is None:
         message = "Такой группы нет"
-        return vk_api.send_message(user_info, TOKEN, message)
+        return vk_group_api.send_message(user_info, TOKEN, message)
     db.session.delete(group)
     db.session.commit()
     message = "Группа удалена"
-    return vk_api.send_message(user_info, TOKEN, message)
+    return vk_group_api.send_message(user_info, TOKEN, message)
 
     
 def post_memes_handler(user_info, TOKEN, vk_response):
     groups_letter_id = Mems.query.order_by(Mems.group_id)
     for group_letter_id in groups_letter_id:
         group_number_id = vk_api.get_group_info(str(group_letter_id)[16:-1])
-        post_id = vk_api.parse_posts(group_number_id)
+        post_id = vk_user_api.parse_posts(group_number_id)
         message = 'https://vk.com/wall-%s_%s' % (group_number_id, post_id)
-        vk_api.send_message(user_info, TOKEN, message) 
+        vk_group_api.send_message(user_info, TOKEN, message) 
 
 
 def post_memes_from_handler(user_info, token):
