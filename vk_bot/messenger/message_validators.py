0e55ff988185dc0370380ae36bae1bc1b9b27b78
mcproger <artem7lomonosov@gmail.com>
2017-07-18 13:13:10 +0300

    Fix weather validator

--- vk_bot/messenger/message_validators.py
+++ vk_bot/messenger/message_validators.py
@@ -53,7 +53,7 @@ def is_parse_hidden_info_command(messaging_event):
     return validate(validators, messaging_event)
 
 
-def is_post_weater_command(messaging_event):
+def is_post_weather_command(messaging_event):
     validators = ['Погода','Погоду']
     return validate(validators, messaging_event)
 
