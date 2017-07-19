8e18bd7b67f48816b670ea02bea805038dbb6003
mcproger <artem7lomonosov@gmail.com>
2017-07-19 16:55:16 +0300

    Upgrade database models

--- vk_bot/models/groups.py
+++ vk_bot/models/groups.py
@@ -1,28 +1,23 @@
 from vk_bot.server import db
 
-class News(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(128), unique=True, nullable=False)
+    mems_groups = db.relationship('Mems_group', backref='owner', lazy='dynamic')
+    news_groups = db.relationship('News_group', backref='owner', lazy='dynamic')
+        
+
+class Mems_group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     group_name = db.Column(db.String(128), unique=True, nullable=False)
     group_id = db.Column(db.String(128), unique=True, nullable=False)
+    owner_id = db.Column(db.String, db.ForeignKey('user.user_id'))
 
-    def __init__(self, group_name, group_id):
-        self.group_name = group_name
-        self.group_id = group_id
-
-    def __repr__(self):
-        return '<News %r, %r>' % (self.group_name, self.group_id)
-
-
-class Mems(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    
+class News_group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     group_name = db.Column(db.String(128), unique=True, nullable=False)
     group_id = db.Column(db.String(128), unique=True, nullable=False)
-
-    def __init__(self, group_name, group_id):
-        self.group_name = group_name
-        self.group_id = group_id
-
-    def __repr__(self):
-        return '%r' % (self.group_id)
+    owner_id = db.Column(db.String, db.ForeignKey('user.user_id'))
 
 
