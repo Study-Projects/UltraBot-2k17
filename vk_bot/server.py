fef78babd1ff3ddd7b299e554a7a04c9a520a777
mcproger <artem7lomonosov@gmail.com>
2017-07-20 02:25:03 +0300

    Fix syntax

--- vk_bot/server.py
+++ vk_bot/server.py
@@ -58,7 +58,7 @@ def webhook():
             ),
             (
                 message_validators.is_post_list_of_news_groups_command,
-                message_handlers.is_post_list_of_news_groups_command_handler                                                                                                                                                                                                                                                         
+                message_handlers.post_list_of_news_groups_handler                                                                                                                                                                                                                                                         
             ),
             (
                 message_validators.is_parse_possible_photos_command,
