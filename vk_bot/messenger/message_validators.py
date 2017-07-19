887b15deb02b2198ff57eb047ec5c900617ec642
mcproger <artem7lomonosov@gmail.com>
2017-07-19 18:50:14 +0300

    FIx syntax mistake

--- vk_bot/messenger/message_validators.py
+++ vk_bot/messenger/message_validators.py
@@ -18,7 +18,7 @@ def is_post_memes_from_command(messaging_event):
     return validate(validators, messaging_event)
 
 
-def is_post_list_of_memes_group_command('messaging_event'):
+def is_post_list_of_memes_group_command(messaging_event):
     validators = ['Пришли список мемогрупп']
     return validate(validators, messaging_event)
 
