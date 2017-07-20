9f1533e6effdd5d9152afc436240ac39874b7c5f
Ilya Osipov <070809010@mail.ru>
2017-07-21 03:02:08 +0500

    Rewrite to catch not only ids, but text and attachments

--- vk_bot/vk_user_api.py
+++ vk_bot/vk_user_api.py
@@ -10,4 +10,19 @@ def parse_posts(group_id):
     posts = vk_user_api.wall.get(owner_id=group_id)
     if 'error' in posts:
         return None
-    return posts[1]['id']
+    text = posts[1]['text']
+    attachments_objects = []
+    if 'attachments' in posts[1]:
+        attachments = posts[1]['attachments']
+        for attachment in attachments:
+            if attachment['type'] == 'video':
+                preview_id = attachment['video']['id']
+                preview_owner_id = attachment['video']['owner_id']
+                video = 'video%s_%s' % (preview_owner_id, preview_id)
+                attachments_objects.append(video)
+            if attachment['type'] == 'photo':
+                photo_id = attachment['photo']['id']
+                photo_owner_id = attachment['photo']['owner_id']
+                photo = 'photo%s_%s' % (photo_owner_id, photo_id)
+                attachments_objects.append(photo)
+    return text, attachments_objects
