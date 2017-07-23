c83d70f265f210d4a4ee67ab9c604ebca60b60a6
mcproger <artem7lomonosov@gmail.com>
2017-07-23 15:36:18 +0300

    Fix syntax

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
+TOKEN = os.environ['TOKEN']
+CONFIRMATION_TOKEN = os.environ['CONFIRMATION_TOKEN']
 APP_ID = os.environ['APP_ID']
 LOGIN = os.environ['LOGIN']
 PASSWORD = os.environ['PASSWORD']
 BOT_GROUP_ID = os.environ['BOT_GROUP_ID']
-TOKEN = os.environ['TOKEN']
-CONFIRMATION_TOKEN = os.environ['CONFIRMATION_TOKEN']
 WEATHER_KEY = os.environ['WEATHER_KEY']
