f137ee80867563c92d0a31cebf06b92a84ba7341
mcproger <artem7lomonosov@gmail.com>
2017-07-24 23:04:53 +0300

    Add server tests

--- tests/server_test.py
+++ tests/server_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
-import os
+import os, json
 
 
 from vk_bot import server
@@ -8,7 +8,11 @@ from vk_bot import server
 
 class MessageHandlersTestCase(TestCase):
     def setUp(self):
-        pass
+        self.TOKEN = 1
+        self.user_info = 'USER_ID'
+
+        server.app.config['TOKEN'] = self.TOKEN
+        self.app = server.app.test_client()
 
 
     def tearDown(self):
@@ -26,9 +30,15 @@ class MessageHandlersTestCase(TestCase):
         return vk_messaging_event
 
 
-    def generate_message_new(self):
+    def generate_confirmation(self):
+    	message_type = 'confirmation'
+    	body = 'Doesn\'t matter'
+    	return self.generate_vk_messaging_event(message_type, body)
+
+
+    def generate_message_new(self, message):
         message_type = 'message_new'
-        body = 'Do some db action'
+        body = message
         return self.generate_vk_messaging_event(message_type, body)
 
 
@@ -36,5 +46,66 @@ class MessageHandlersTestCase(TestCase):
         return message
 
 
-    def test_webhook(self):
-    	pass
+    @patch('vk_bot.server.message_handlers')
+    def test_add_group_handler_gets_called(self, message_handlers_mock):
+    	message_handlers_mock.add_group_handler = MagicMock()
+    	vk_messaging_event = self.generate_message_new("Добавь мемогруппу memes memes")
+    	self.app.post('/', data=json.dumps(vk_messaging_event))
+    	number_of_calls = message_handlers_mock.add_group_handler.call_count
+    	self.assertEqual(number_of_calls, 1)
+
+
+
+    @patch('vk_bot.server.message_handlers')
+    def test_delet_group_handler_gets_called(self, message_handlers_mock):
+    	message_handlers_mock.add_group_handler = MagicMock()
+    	vk_messaging_event = self.generate_message_new("Удали мемогруппу memes memes")
+    	self.app.post('/', data=json.dumps(vk_messaging_event))
+    	number_of_calls = message_handlers_mock.delete_group_handler.call_count
+    	self.assertEqual(number_of_calls, 1)
+
+
+    @patch('vk_bot.server.message_handlers')
+    def test_post_handler_gets_called(self, message_handlers_mock):
+    	message_handlers_mock.add_group_handler = MagicMock()
+    	vk_messaging_event = self.generate_message_new("Пришли свежие мемы")
+    	self.app.post('/', data=json.dumps(vk_messaging_event))
+    	number_of_calls = message_handlers_mock.post_handler.call_count
+    	self.assertEqual(number_of_calls, 1)
+
+
+    @patch('vk_bot.server.message_handlers')
+    def test_post_from_handler_gets_called(self, message_handlers_mock):
+    	message_handlers_mock.add_group_handler = MagicMock()
+    	vk_messaging_event = self.generate_message_new("Пришли новости из")
+    	self.app.post('/', data=json.dumps(vk_messaging_event))
+    	number_of_calls = message_handlers_mock.post_from_handler.call_count
+    	self.assertEqual(number_of_calls, 1)
+
+
+    @patch('vk_bot.server.message_handlers')
+    def test_post_list_of_groups_handler_gets_called(self, message_handlers_mock):
+        message_handlers_mock.add_group_handler = MagicMock()
+        vk_messaging_event = self.generate_message_new("Пришли список новостигрупп")
+        self.app.post('/', data=json.dumps(vk_messaging_event))
+        number_of_calls = message_handlers_mock.post_list_of_groups_handler.call_count
+        self.assertEqual(number_of_calls, 1)
+
+
+    @patch('vk_bot.server.message_handlers')
+    def test_delete_all_groups_handler_gets_called(self, message_handlers_mock):
+        message_handlers_mock.add_group_handler = MagicMock()
+        vk_messaging_event = self.generate_message_new("Удали все")
+        self.app.post('/', data=json.dumps(vk_messaging_event))
+        number_of_calls = message_handlers_mock.delete_all_groups_handler.call_count
+        self.assertEqual(number_of_calls, 1)
+
+
+    @patch('vk_bot.server.message_handlers')
+    def test_post_weather_handler_gets_called(self, message_handlers_mock):
+        message_handlers_mock.add_group_handler = MagicMock()
+        vk_messaging_event = self.generate_message_new("Погода")
+        self.app.post('/', data=json.dumps(vk_messaging_event))
+        number_of_calls = message_handlers_mock.post_weather_handler.call_count
+        self.assertEqual(number_of_calls, 1)              		    
+    	
