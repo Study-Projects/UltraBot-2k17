edf7db279570ed72fc4c8584678da23d8d844798
Ilya Osipov <070809010@mail.ru>
2017-07-24 16:19:00 +0500

    Make weather more pretty

--- vk_bot/weather_api.py
+++ vk_bot/weather_api.py
@@ -4,4 +4,20 @@ import urllib.request
 def fetch_weather(weather_key, city):
     url = 'https://api.apixu.com/v1/current.json?key=%s&q=%s' % (weather_key, city)
     site = urllib.request.urlopen(url)
-    return site.read()
+    weather = site.read()
+    current_weather = weather["current"]
+    last_updated = current_weather["last_updated"]
+    temp = current_weather["temp_c"]
+    text = current_weather["condition"]["text"]
+    wind = current_weather["wind_kph"]
+    cloud = current_weather["cloud"]
+    feelslike = current_weather["feelslike_c"]
+    text_weather = """
+    Погода проверялась: %s
+    На улице %s
+    Температура: %s
+    Чувствуется: %s
+    Облачность: %s
+    Ветер: %s
+    """ % (last_updated, text, temp, feelslike, cloud, wind)
+    return text_weather
