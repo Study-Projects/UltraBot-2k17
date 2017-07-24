dd4d57ba51c1c2a778a353ff2fb5635f6c131e2f
mcproger <artem7lomonosov@gmail.com>
2017-07-24 16:56:57 +0300

    Add separation of tokens

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 import os
 
+#testing db
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#vk comunity tokens
 TOKEN = os.environ.get('TOKEN')
 CONFIRMATION_TOKEN = os.environ.get('CONFIRMATION_TOKEN')
+BOT_GROUP_ID = os.environ.get('BOT_GROUP_ID')
+
+#vk user tokens
 APP_ID = os.environ.get('APP_ID')
 LOGIN = os.environ.get('LOGIN')
 PASSWORD = os.environ.get('PASSWORD')
-BOT_GROUP_ID = os.environ.get('BOT_GROUP_ID')
+
+#weather token
 WEATHER_KEY = os.environ.get('WEATHER_KEY')
